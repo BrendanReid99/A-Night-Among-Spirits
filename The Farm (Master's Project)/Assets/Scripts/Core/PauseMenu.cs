78b34fd7b3561b0a17d77680363074fc818e1585
Brendan Reid <36493852+BrendanReid99@users.noreply.github.com>
2023-08-17 22:44:52 +0100

    font added and control menu finished
    -> control menu needs updated with what I sent greg.

--- The Farm (Master's Project)/Assets/Scripts/Core/PauseMenu.cs
+++ The Farm (Master's Project)/Assets/Scripts/Core/PauseMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseCanvas;
+    public GameObject controlsCanvas;
 
     public void resumeGame() { 
         
@@ -18,5 +19,26 @@ public class PauseMenu : MonoBehaviour
 
     public void exitGame() { 
         Application.Quit();
+    }
+
+    public void controlsMenu()
+    {
+        Time.timeScale = 0;
+        Debug.Log("controls menu clicked");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseCanvas.SetActive(false);
+        controlsCanvas.SetActive(true);
+    }
+
+    public void backButton()
+    {
+        Time.timeScale = 0;
+        Debug.Log("back button clicked");
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        controlsCanvas.SetActive(false);
+        pauseCanvas.SetActive(true);
+        
     }
 }
