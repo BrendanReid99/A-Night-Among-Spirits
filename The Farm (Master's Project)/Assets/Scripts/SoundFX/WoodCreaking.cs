7c81e79da78b1e979d5686acbf39fd854ea3909c
Brendan Reid <36493852+BrendanReid99@users.noreply.github.com>
2023-08-17 20:28:28 +0100

    random wood creak script added

---
+++ The Farm (Master's Project)/Assets/Scripts/SoundFX/WoodCreaking.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodCreaking : MonoBehaviour
+{
+    private float timer = 0.0f;
+    private float interval = 0.0f;
+
+    //[SerializeField] private GameObject directionalLight;
+
+    public AudioSource woodCreak;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        woodCreak = GetComponent<AudioSource>();
+        interval = Random.Range(25.0f, 75.0f);
+        Debug.Log(interval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > interval)
+        {
+            Debug.Log("Wood Creak TRUE");
+            StartCoroutine(woodCreaking());
+            timer = 0.0f;
+            interval = Random.Range(25.0f, 75.0f);
+        }
+    }
+
+    private IEnumerator woodCreaking()
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        woodCreak.Play();
+
+    }
+}
